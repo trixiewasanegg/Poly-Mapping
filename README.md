e6c7a4feffd7a1cdba838348a5b6a8672f9caa32
Trixie <bea@lab-bea.com>
2025-05-17 12:52:57 +0800

    Feature Updates

--- README.md
+++ README.md
@@ -1,18 +1,30 @@
 # Poly-Mapping
 Polycule Mapping Using Dash Cytoscapes, akin to Polycul.Es.
+Grouping, highlighting of connected nodes when selecting edges & vice versa.
+
+# TODO
+- Currently data is stored in multiple CSV files, need to build in JSON import.
 
 # Dependencies
 Pip managed:
+- dash
 - dash_cytoscape
 
 # Template Data
 ## TestData-Simple.csv (git ignored)
 CSV file with 3 columns for test data
-- Node 1 - Name (str)
-- Node 2 - Name (str)
-- RelType - Relationship Type (str)
+- ID - Unique ID
+- Node 1 - Name, must be on testNodes list
+- Node 2 - Name, must be on testNodes list
+- RelType - Relationship Type
 
-## TestColors-Simple.csv (git ignored)
+## TestStyles-Simple.csv (git ignored)
 CSV file with 2 columns for colors
-- RelType - matches TestData types
+- RelType - matches TestData RelType
 - Hex - hex color
+
+## TestNodes-Simple.csv (git ignored)
+CSV file with 3 columns
+- Name - Name of the node
+- Class - Group or Person
+- Group - Group this node falls under, or "None" if not
--- main.py
+++ main.py
@@ -1,89 +1,71 @@
-# Python modules
-import os
-import datetime
-import fnmatch
-import string
-import random
-
-# Mapping
-from dash import Dash, html
+# Imports
+from dash import Dash, html, callback, Input, Output
 import dash_cytoscape as cyto
 
-class color:	
-	def redistribute(r, g, b):
-		#Sets max value
-		threshold = 255.999
-		
-		#Gets max value, if so returns (r,g,b)
-		m = max(r,g,b)
-		if m <= threshold:
-			return int(r),int(g),int(b)
-		
-		#If pure white, return 255,255,255
-		total = r+g+b
-		if total >= 3*threshold:
-			return int(threshold),int(threshold),int(threshold)
-		
-		# Gets color ratios & redistributes max value down
-		x = (3 * threshold - total) / (3 * m - total)
-		gray = threshold - x * m
-		return int(gray + x * r), int(gray + x * g), int(gray + x * b)
-	
-	# Lightens hex value by factor
-	def lighten(hexa, factor):
-		r = int(hexa[0:2],16)
-		g = int(hexa[2:4],16)
-		b = int(hexa[4:6],16)
-
-		r = r * factor
-		g = g * factor
-		b = b * factor
-
-		rgb = color.redistribute(r,g,b)
-
-		hexa = '%02x%02x%02x' % rgb
-
-		return hexa
-
 testDataFile = "TestData-Simple.csv"
-testColorFile = "TestColors-Simple.csv"
+testStyleFile = "TestStyles-Simple.csv"
+testNodeFile = "TestNodes-Simple.csv"
+
+nodes = []
+with open(testNodeFile) as csv:
+    lines = csv.readlines()
+    for line in lines[1:]:
+        vals = line.replace('\n','').split(",")
+        node = {'data': {'id': vals[0],'label': vals[0],'classes': vals[1]}}
+        if str(vals[2]) != 'None':
+            node['data']['parent'] = vals[2]
+
+        nodes.append(node)
+
 
 links = []
-nodes = set()
+nodeConnections = dict()
 
 # Reads CSV
 with open(testDataFile) as csv:
     lines = csv.readlines()
     for line in lines[1:]:
         vals = line.replace('\n','').split(",")
-        node1 = vals[0]
-        node2 = vals[1]
-        relType = vals[2]
-        link = {'node1': node1, 'node2': node2, 'relType': relType}
+        edgeID = vals[0]
+        node1 = vals[1]
+        node2 = vals[2]
+        relType = vals[3]
+        link = {'id': edgeID,'node1': node1, 'node2': node2, 'relType': relType}
         links.append(link)
-        nodes.add(node1)
-        nodes.add(node2)
+        try:
+            nodeConnections[node1].add(edgeID)
+        except:
+            nodeConnections[node1] = set()
+            nodeConnections[node1].add(edgeID)
+        
+        try:
+            nodeConnections[node2].add(edgeID)
+        except:
+            nodeConnections[node2] = set()
+            nodeConnections[node2].add(edgeID)
 
-colors = []
+nodeSize = 20
 
-with open(testColorFile) as csv:
+stylePredef = [
+    {'selector': 'node', 'style': {'content': 'data(label)', 'z-index': 999, 'width': nodeSize, 'height': nodeSize}},
+    {'selector': 'edge', 'style': {'curve-style': 'round-segments', 'width': 2}},
+    {'selector': 'edge:selected', 'style': {'content': 'data(label)', 'width': 5}}
+]
+
+with open(testStyleFile) as csv:
     lines = csv.readlines()
     for line in lines[1:]:
         vals = line.replace('\n','').split(",")
-        normal = {'selector': '.'+vals[0], 'style': {'line-color': "#"+vals[1], 'width': 2}}
-        selected = {'selector': '.'+vals[0]+':selected', 'style': {'width': 5}}
-        colors.append(normal)
-        colors.append(selected)
+        style = {'selector': '.'+vals[0], 'style': {'line-color': "#"+vals[1],'background-color': "#"+vals[1]}}
+        stylePredef.append(style)
+
+styles = stylePredef
 
 # Pulls from links & nodes, generates cytoscape elements
-cytoscapeElements = []
-
-for node in nodes:
-    element = {'data' : {'id': node, 'label': node}}
-    cytoscapeElements.append(element)
+cytoscapeElements = nodes
 
 for link in links:
-    element = {'data': {'source': link['node1'], 'target': link['node2'], 'label': link['relType']}, 'classes': link['relType']}
+    element = {'data': {'id': link['id'],'source': link['node1'], 'target': link['node2'], 'label': link['relType']}, 'classes': link['relType']}
     cytoscapeElements.append(element)
 
 app = Dash()
@@ -94,14 +76,45 @@ app.layout = html.Div([
     cyto.Cytoscape(
         id='primaryMap',
         elements=cytoscapeElements,
+        responsive=True,
         layout={'name': 'dagre', 'fit': True, 'nodeSep': 200},
         style={'width': '100vw', 'height': '100vh'},
-        stylesheet = [
-            {'selector': 'node', 'style': {'content': 'data(label)'}},
-            {'selector': 'edge:selected', 'style': {'content': 'data(label)'}},
-        ] + colors
+        stylesheet = styles,
     )
 ])
+
+@callback(
+    Output("primaryMap", "stylesheet",allow_duplicate=True),
+    Input("primaryMap", "selectedNodeData"),
+    Input("primaryMap", "selectedEdgeData"),
+    prevent_initial_call=True,
+)
+def selectNode(nodes, edges):
+    print(f"Node/s selected: {nodes}")
+    newStyles = []
+    if nodes != [] and nodes != None:
+        for node in nodes:
+            if node['classes'] == 'Group':
+                return stylePredef
+            else:
+                key = node['id']
+                connections = nodeConnections[key]
+                print(f'Connections: {connections}')
+                
+                for con in connections:
+                    newStyles.append({'selector': '#'+con, 'style': {'width': 5, 'content': 'data(label)'}})
+
+    print(f"Edge/s selected: {edges}")
+    largeNode = nodeSize * 1.25
+    if edges != [] and edges != None:
+        for edge in edges:
+            for node in [edge['source'], edge['target']]:
+                newStyles.append(
+                    {'selector': "#"+node, 'style': {'width': largeNode, 'height': largeNode}}
+                )
+
+    return stylePredef + newStyles
+
 
 if __name__ == '__main__':
     app.run(debug=True)
